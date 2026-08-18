7f80896a3a448efebe786dce1fa9c3d08afd7254
ionide <mar109us@gmail.com>
2026-08-18 21:57:39 +0200

    messing with svg polygons

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 addEventListener("click", (event) => {
    if (event.target.parentNode.id === "left-content") {
-      console.log(event.target.id);
+      console.log("[click event]", event.target.parentNode.id);
       let section = "left";
       actionContent(section, event);
    }
    if (event.target.parentNode.id === "middle-content") {
-      console.log(event.target.id);
+      console.log("[click event]", event.target.parentNode.id);
       let section = "middle";
       actionContent(section, event);
    }
    if (event.target.parentNode.id === "right-content") {
-      console.log(event.target.id);
+      console.log("[click event]", event.target.parentNode.id);
       let section = "right";
       actionContent(section, event);
    }
@@ -18,12 +18,25 @@ addEventListener("click", (event) => {
 
 function actionContent(section, event) {
    if (section === "left") {
-      console.log("mew");
+      console.log("[captured event]", event.target.id);
+      addPoint();
    }
    if (section === "middle") {
-      console.log("mew1");
+      console.log("[captured event]", event.target.id);
+      addPoint();
    }
    if (section === "right") {
-      console.log("mew2");
+      console.log("[captured event]", event.target.id);
+      addPoint();
    }
+}
+
+let pointAmount = 0;
+let pointIncreaseAmount = 10;
+
+function addPoint() {
+   pointAmount += pointIncreaseAmount;
+   document.getElementById("data-1").innerText = pointAmount;
+   document.getElementById("svg").innerHTML = `
+      <polygon points="100 100, 400 100, 250 400"/>`;
 }
