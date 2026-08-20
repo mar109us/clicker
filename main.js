c277b58c5ddcbb22bf54c2355b9b2efbec2d0dba
ionide <mar109us@gmail.com>
2026-08-20 13:57:41 +0200

    simple point system implementation, pricing point buy system for adding polygon

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+const displayMainPoints = document.getElementById("data-1");
+const displayPolygonPoints = document.getElementById("data-2");
+const display3 = document.getElementById("data-3");
+const display4 = document.getElementById("data-4");
+
+const addPolygonButton = document.getElementById("button-add-polygon");
+
+let increasePolygonPrice = 100;
+let pointAmount = 0;
+let pointIncreaseAmount = 10;
+
+let polygon = "";
+
+let randomPolygonPoint1 = 0;
+let randomPolygonPoint2 = 0;
+
+let randomColor1;
+let randomColor2;
+let randomColor3;
+
 addEventListener("click", (event) => {
    if (event.target.parentNode.id === "left-content") {
       console.log("[click event]", event.target.parentNode.id);
@@ -23,41 +43,72 @@ function actionContent(section, event) {
    }
    if (section === "middle") {
       console.log("[captured event]", event.target.id);
-      addPoint();
    }
    if (section === "right") {
       console.log("[captured event]", event.target.id);
-      addPoint();
+      buyUpgrade(event.target.id)
    }
 }
 
-const displayMainPoints = document.getElementById("data-1");
-const displayPolygonPoints = document.getElementById("data-2");
-const display3 = document.getElementById("data-3");
-const display4 = document.getElementById("data-4");
-
 function updateView() {
-   displayMainPoints.innerText = pointAmount
+   displayMainPoints.innerText = pointAmount;
+   updateSVG();
+   checkPrice();
+}
+updateView();
+
+function checkPrice() {
+   if (pointAmount < increasePolygonPrice) {
+      addPolygonButton.disabled = true;
+   }
+   if (pointAmount >= increasePolygonPrice) {
+      addPolygonButton.disabled = false;
+   }
 }
 
-let pointAmount = 0;
-let pointIncreaseAmount = 10;
+function buyUpgrade(id) {
+   if (id === "button-add-polygon") {
+      pointAmount -= increasePolygonPrice
+   }
+   updateSVGData()
+}
 
 function addPoint() {
    pointAmount += pointIncreaseAmount;
-   updateView()
+   updateView();
 }
 
-let polygon = "";
+function updateSVGData() {
+   if (pointAmount < 100) {
+      randomPolygonPoint1 = Math.floor(Math.random() * 50) + 450;
+      randomPolygonPoint2 = Math.floor(Math.random() * 50) + 450;
+   } else if (pointAmount < 200) {
+      randomPolygonPoint1 = Math.floor(Math.random() * 100) + 350;
+      randomPolygonPoint2 = Math.floor(Math.random() * 100) + 350;
+   } else if (pointAmount < 300) {
+      randomPolygonPoint1 = Math.floor(Math.random() * 200) + 250;
+      randomPolygonPoint2 = Math.floor(Math.random() * 200) + 250;
+   } else if (pointAmount < 400) {
+      randomPolygonPoint1 = Math.floor(Math.random() * 300) + 150;
+      randomPolygonPoint2 = Math.floor(Math.random() * 300) + 150;
+   } else if (pointAmount < 500) {
+      randomPolygonPoint1 = Math.floor(Math.random() * 400) + 50;
+      randomPolygonPoint2 = Math.floor(Math.random() * 400) + 50;
+   } else {
+      randomPolygonPoint1 = Math.floor(Math.random() * 500) + 10;
+      randomPolygonPoint2 = Math.floor(Math.random() * 600);
+   }
 
-function createSVG() {
-   let randomPolygonPoint1 = Math.floor(Math.random() * 500) + 10;
-   let randomPolygonPoint2 = Math.floor(Math.random() * 600);
-
-   let randomColor1 = Math.floor(Math.random() * 85) + 170;
-   let randomColor2 = Math.floor(Math.random() * 55) + 200;
-   let randomColor3 = Math.floor(Math.random() * 45) + 210;
    polygon += `, ${randomPolygonPoint1} ${randomPolygonPoint2}`;
+
+   randomColor1 = Math.floor(Math.random() * 85) + 170;
+   randomColor2 = Math.floor(Math.random() * 55) + 200;
+   randomColor3 = Math.floor(Math.random() * 45) + 210;
+
+   updateView();
+}
+
+function updateSVG() {
    document.getElementById("svg").innerHTML = `
    <defs>
     <linearGradient id="Gradient1">
@@ -80,6 +131,6 @@ function createSVG() {
         fill-opacity="0.9" />
     </pattern>
   </defs>
-    <polygon points="200 300, 300 200 ${polygon}" fill="url(#Pattern)"/>
+    <polygon points="200 300, 300 200, 100 200 ${polygon}" fill="url(#Pattern)"/>
     `;
 }
